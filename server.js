9a99d8343dd973b16b56adf5b0732cf865452f82
gabriel.dimitrov <Gabriel.Dimitrov@externals.adidas-group.com>
2019-02-10 20:29:39 +0200

    Add heroku settings

--- server.js
+++ server.js
@@ -37,6 +37,16 @@ cron.schedule('0 11 * * *', () => {
 }, {
     timezone: 'Europe/Sofia'
 })
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+
 app.listen(port, () => {
     console.log(`server up and running on port ${port}`)
 });
