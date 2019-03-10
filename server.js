a5f0b9078a44845e94ef03aad7c1774dd6f45af8
Gabriel Dimitrov <gabriel.dimitrov@externals.adidas.com>
2019-03-10 22:48:38 +0200

    asd

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const requireAuth = passport.authenticate('jwt', {session: false})
 const requireSignIn = passport.authenticate('local', {session: false})
 const app = express()
 const port = process.env.PORT || 5000
-const mongoURL = process.envMONGODB_URI ||'mongodb://localhost/service-alex'
+const mongoURL = process.env.MONGODB_URI ||'mongodb://localhost/service-alex'
 
 mongoose.connect(mongoURL)
 
