9f1655ed7d136f9856829c80d2ced089f620ef9d
Dimitrov <gabriel.dimitrov@news.co.uk>
2019-12-07 15:14:05 +0200

    Update SMS message

--- aws-test.js
+++ aws-test.js
@@ -18,9 +18,11 @@ customerController.getAllExpiringReviews((customers, Customer) => {
     }
 
     var params = {
-      Message: `Здравейте, от сервиз алекс искаме да ви съобщим, че прегледа на автомобил с регистрационен номер ${customer.carNumber} изтича след 7 дни.`, /* required */
+      Message: `Здравейте!
+      ПГТП "Alex" ви уведомява, че прегледа на автомобил с регистрационен номер ${customer.carNumber} изтича след 7 дни.`, /* required */
       PhoneNumber: `+359${customer.phoneNumber}`,
     };
+
     // Create promise and SNS service object
     var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
 
